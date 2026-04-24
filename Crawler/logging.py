772b0993084ce64d9627e12c4c5ff91e69a5594a
charocon <ocon.char22@gmail.com>
2026-04-23 22:04:56 -0700

    initial setup

---
+++ Crawler/logging.py
@@ -0,0 +1,26 @@
+import scrapy
+
+def log_page(count, url):
+    print(f"Crawled {count}: {url}")
+
+# testing different seeds
+def seed_testing():
+    # set up 15 different seeds for testing
+    print("todo")
+
+# verifying output folder fills
+def output_folder_testing():
+    # check that the folder fills with 
+    print("todo")
+
+# checking for crashes
+def proper_crawl_test():
+    # print reports of iterations, iterations match up with indexing of pages, proper amount of pages are indexed
+    print("todo")
+
+
+def main():
+    print("run testing for all")
+
+if __name__ == "__main__":
+    main()
