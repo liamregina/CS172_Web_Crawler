552303535b127c733c5f8883de9bfbfe6bf50979
charocon <ocon.char22@gmail.com>
2026-05-21 10:49:44 -0700

    search webpage part

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,10 +1,34 @@
 # Windows: .\.venv\Scripts\python.exe -m flask --app frontend.frontend run
+
+# flask --app flaskr run --debug
+
 # Open: http://127.0.0.1:5000
+
+import os
 
 from flask import Flask
 
-app = Flask(__name__)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
