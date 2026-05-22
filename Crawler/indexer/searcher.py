a7ebbe9eef56f502087c2d4a0996d16f163bf867
GokulNookula <gokulknookula@gmail.com>
2026-05-21 21:06:07 -0700

    merging branches with kim's searcher.py code and indexer.py code

--- Crawler/indexer/searcher.py
+++ Crawler/indexer/searcher.py
@@ -18,7 +18,7 @@ from org.apache.lucene.analysis.standard import StandardAnalyzer
 from org.apache.lucene.queryparser.classic import MultiFieldQueryParser
 
 # Local
-from config import INDEX_DIR, TOP_K, FIELD_BOOSTS, TITLE_FIELD, URL_FIELD, BODY_FIELD, HTML_FILE_FIELD
+from Crawler.indexer.config import INDEX_DIR, TOP_K, FIELD_BOOSTS, TITLE_FIELD, URL_FIELD, BODY_FIELD, HTML_FILE_FIELD
 from snippet import get_snippet
 
 def init_jvm() -> None:
--- Indexer/__init__.py
+++
@@ -1,3 +0,0 @@
-# Indexer/__init__.py
-# Makes Indexer/ a Python package so other files can import from it
-# e.g. from Indexer.searcher import search
