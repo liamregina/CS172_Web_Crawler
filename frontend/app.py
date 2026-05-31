25db80c4ab6f5ed3e437273e01643577b51a268b
charocon <ocon.char22@gmail.com>
2026-05-30 18:04:56 -0700

    okay it works now atleast on my machine

--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,17 @@
 # Windows: .\.venv\Scripts\python.exe -m flask --app frontend run --debug
 # Macbook: ./.venv/bin/python -m flask --app frontend run --debug
 
+# In WSL ubuntu : python -m flask --app frontend.app:app run --debug
+
 # Open: http://127.0.0.1:5000/
 
 from flask import Flask, request, render_template, url_for, redirect
+import lucene
+from Indexer.searcher import search as lucene_search
 
 app = Flask(__name__, static_folder='frontend/static', static_url_path='/static')
+
+lucene.initVM(vmargs=['-Djava.awt.headless=true'])
 
 @app.route("/", methods=['GET'])
 def search():
@@ -13,9 +19,10 @@ def search():
 
 @app.route("/results", methods=['POST'])
 def results():
+    lucene.getVMEnv().attachCurrentThread()
     query_search = request.form.get('query')
 
-    # Pylucune part
+    # Pylucene part
 
     # User click search, you should show a list of results (e.g., first 10) returned by Lucene.
     # List should be ordered in decreasing order of score.
@@ -23,7 +30,8 @@ def results():
     # Do not use SOLR or another framework that automatically builds the
     # UI for you
 
-    return render_template("results.html")
+    indexer_query_output = lucene_search(query_search)
+    return render_template("results.html", output=indexer_query_output, query=query_search)
 
 
 if __name__ == "__main__":
